57b586ced338ab42181967fcda1e126ee65feb60
Gabriel <mutatisguga@gmail.com>
2015-09-01 16:33:20 -0300

    f

---
+++ Assets/Resources/tranforma.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tranforma : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector2(transform.position.x - 1500, transform.position.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
